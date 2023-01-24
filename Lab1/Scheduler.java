73aeafadf0a46ec1ff007e0832ce8c75e26af78a
Haniya-Ahmed <80060018+Haniya-Ahmed@users.noreply.github.com>
2023-01-24 17:48:32 -0400

    Update Scheduler.java

--- Lab1/Scheduler.java
+++ Lab1/Scheduler.java
@@ -1,7 +1,7 @@
 import java.util.Date;
 
 public abstract class Scheduler {
-    abstract Calendar createCalendar();
+    abstract Calendar createCalendar(); //factory method
     public void schedule(Event event) {
         var today = new Date();
         createCalendar().addEvent(event, today);
