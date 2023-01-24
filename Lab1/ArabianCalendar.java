9913a9948febeebd9ffe1edbb1a485e426a38413
Haniya-Ahmed <80060018+Haniya-Ahmed@users.noreply.github.com>
2023-01-24 17:46:25 -0400

    Update ArabianCalendar.java

--- Lab1/ArabianCalendar.java
+++ Lab1/ArabianCalendar.java
@@ -1,5 +1,6 @@
 import java.util.Date;
 
+//concrete product
 public class ArabianCalendar implements Calendar {
     public void addEvent(Event event, Date date) {
         System.out.println("Adding an event on the given date on Arabian Calendar.");
