2ae1162b1c6856dab36aca4930bb5fb69c478cb6
Haniya-Ahmed <80060018+Haniya-Ahmed@users.noreply.github.com>
2023-01-24 17:46:41 -0400

    Update GregorianCalendar.java

--- Lab1/GregorianCalendar.java
+++ Lab1/GregorianCalendar.java
@@ -1,5 +1,6 @@
 
 import java.util.Date;
+//concrete product
 public class GregorianCalendar implements Calendar {
     public void addEvent(Event event, Date date) {
         System.out.println("Adding an event on the given date on Gregorian Calendar.");
