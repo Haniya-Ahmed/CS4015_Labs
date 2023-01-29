0794ee49b868089000e7c67445085fc4edd64755
Haniya-Ahmed <80060018+Haniya-Ahmed@users.noreply.github.com>
2023-01-29 16:56:42 -0400

    Create Component.java

---
+++ Lab2/Component.java
@@ -0,0 +1,16 @@
+
+//This is the abstract prototype class for components which contains the abstract clone method
+public abstract class Component {
+    /* TODO Implement an appropriate interface method */
+    public String objectName;
+
+    //regular constructor
+    public Component () {}
+
+    //prototype constructor
+    public  Component(Component source){
+        this.objectName = source.objectName;
+    }
+
+    public abstract Component clone();
+}
